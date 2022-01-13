81cebbfd4dd71a895a82658e39e2e89f33d36872
rifat <kautsarrifat@gmail.com>
2022-01-13 18:30:16 +0700

    Adding & updating layout

--- app/src/main/java/com/rifat/megaplex/FavoriteFragment.kt
+++ app/src/main/java/com/rifat/megaplex/FavoriteFragment.kt
@@ -1,16 +1,17 @@
 package com.rifat.megaplex
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
-class AccountFragment : Fragment () {
+class FavoriteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
--- app/src/main/java/com/rifat/megaplex/LoginActivity.kt
+++ app/src/main/java/com/rifat/megaplex/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.rifat.megaplex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnlogin = findViewById<Button>(R.id.btnlogin)
+        btnlogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
